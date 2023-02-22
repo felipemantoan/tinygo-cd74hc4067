7b627a2ccc6aae95f91a24736b822da326b604cd
Luis Felipe Mantoan <luisfelipemantoan@gmail.com>
2023-02-21 21:28:51 -0300

    Stranger

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/felipemantoan/tinygo-cd74hc4067
 
 go 1.19
+
+require tinygo.org/x/drivers v0.24.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"errors"
 	"fmt"
+	"image/color"
 	"machine"
 	"machine/usb/midi"
 	"time"
+
+	"tinygo.org/x/drivers/ws2812"
 )
 
 type Cd74hc4067 struct {
@@ -17,10 +20,10 @@ type Cd74hc4067 struct {
 	S4      machine.Pin
 }
 
-func (mux *Cd74hc4067) GetSignal(pin int) (bool, error) {
+func (mux *Cd74hc4067) GetSignal(pin int) (int, bool, error) {
 
 	if pin > 15 {
-		return false, errors.New(fmt.Sprintf("Invalid pin %d", pin))
+		return -2, false, errors.New(fmt.Sprintf("Invalid pin %d", pin))
 	}
 
 	mux.S1.Set(pins[pin][0] == 1)
@@ -30,9 +33,7 @@ func (mux *Cd74hc4067) GetSignal(pin int) (bool, error) {
 
 	time.Sleep(10 * time.Microsecond)
 
-	value := mux.Signal.Get()
-
-	return value, nil
+	return pin, mux.Signal.Get(), nil
 }
 
 var pins = [][]uint8{
@@ -73,6 +74,8 @@ var notes = []midi.Note{
 	midi.D5,
 }
 
+var Midi = midi.New()
+
 func main() {
 
 	machine.D6.Configure(machine.PinConfig{Mode: machine.PinInputPulldown})
@@ -81,6 +84,13 @@ func main() {
 	machine.D9.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.D8.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.D7.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	machine.NEOPIXEL.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.NEOPIXEL_POWER.Configure(machine.PinConfig{Mode: machine.PinOutput})
+	machine.NEOPIXEL_POWER.High()
+
+	ws := ws2812.New(machine.NEOPIXEL)
+	color := []color.RGBA{{R: 0, G: 255, B: 255, A: 1}}
 
 	mux := &Cd74hc4067{
 		Signal:  machine.D6,
@@ -91,27 +101,31 @@ func main() {
 		S4:      machine.D7,
 	}
 
-	m := midi.New()
+	active := make([]bool, 16)
 
 	for {
 
+		ws.WriteColors(color)
+
 		for index := range pins {
 
-			value, error := mux.GetSignal(index)
+			pin, value, error := mux.GetSignal(index)
 
 			if error != nil {
 				println("Deu bosta")
 			}
 
-			if value {
-				m.NoteOn(0, 0, notes[index], 0x40)
-				println(value, index, notes[index])
+			if value == active[pin] {
 				continue
-			} else {
-				m.NoteOff(0, 0, notes[index], 0x40)
 			}
 
+			active[pin] = value
+
+			if active[pin] {
+				Midi.NoteOn(0, 0, notes[pin], 0x40)
+			} else {
+				Midi.NoteOff(0, 0, notes[pin], 0x40)
+			}
 		}
 	}
-
 }
